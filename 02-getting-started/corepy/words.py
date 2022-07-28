993193893b0e3db718b9d638ca8734c4dccfa44e
watkins-joe <watkinsjoe99@live.com>
2022-07-28 16:07:32 -0500

    added two blank lines between top-level functions

--- 02-getting-started/corepy/words.py
+++ 02-getting-started/corepy/words.py
@@ -1,6 +1,7 @@
 import sys
 from urllib.request import urlopen
 
+
 def fetch_words(url):
     story = urlopen(url)
     story_words = []
@@ -13,9 +14,11 @@ def fetch_words(url):
     story.close()
     return story_words
 
+
 def print_items(items):
     for item in items:
         print(item)
+
 
 def main(url):
     words = fetch_words(url)
